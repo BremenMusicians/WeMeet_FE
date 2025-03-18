919fe7253556ae3dbb8133c034b7dd5f85dc5e40
HYUNA <phyunaa0525@gmail.com>
2025-03-18 18:58:58 +0900

    chore :: 프로필 카드

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,25 +1,16 @@
 import styled from "styled-components"
-import { Accept, AddFriend, Chat, More, Profile, Refusal } from "../assets";// api연동시 기본 이미지 넘어오면 삭제
-import { useState } from "react";
-import { DeleteFriend } from "./DeleteFriend";
+import { Profile } from "../assets";// api연동시 기본 이미지 넘어오면 삭제
+import React from "react";
 
 interface ProfileCard {
-    type: "add" | "accept" | "chat"
     name: string;
     introduce: string;
     position: string[]; // api 연동 시 enum으로 수정
     profileImg?: string;
-    onClick: () => void;
-    onClickEvent?: () => void
+    children: React.ReactNode
 }
 
-export const ProfileCard = ({ type, name, introduce, position, profileImg, onClick, onClickEvent }: ProfileCard) => {
-    const [deleteFriend, setDeleteFriend] = useState<boolean>(false);
-
-    const handleDeleteFriend = () => {
-        setDeleteFriend(false)
-        //api연동
-    }
+export const ProfileCard = ({ name, introduce, position, profileImg, children }: ProfileCard) => {
 
     return (
         <ProfileContainer>
@@ -35,17 +26,7 @@ export const ProfileCard = ({ type, name, introduce, position, profileImg, onCli
                     <Introduce>{introduce}</Introduce>
                 </Column>
             </Flex>
-            {type === "add" && <ClickOption src={AddFriend} onClick={onClick} />}
-            {type === "accept" && onClickEvent &&
-                <RightContainer>
-                    <ClickOption src={Accept} onClick={onClick} />
-                    <ClickOption src={Refusal} onClick={onClickEvent} />
-                </RightContainer>}
-            {type === "chat" && <RightContainer>
-                <ClickOption src={Chat} onClick={onClick} />
-                <MoreIcon src={More} onClick={() => setDeleteFriend(true)} />
-                {deleteFriend && <DeleteFriend onClick={handleDeleteFriend} />}
-            </RightContainer>}
+            {children}
         </ProfileContainer>
     )
 }
@@ -57,11 +38,6 @@ const ProfileContainer = styled.div`
     position: relative;
 `
 
-const ClickOption = styled.img`
-    width: 44px;
-    height: 44px;
-    cursor: pointer;
-`
 const Flex = styled.div`
     display: flex;
     gap: 8px;
@@ -99,17 +75,3 @@ const ProfileImg = styled.img`
     height: 68px;
     border: 1px solid ${({ theme }) => theme.color.gray100};
 `
-
-const RightContainer = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 16px;
-    justify-content: space-between;
-`
-
-const MoreIcon = styled.img`
-    width: 24px;
-    height: 24px;
-    cursor: pointer;
-    margin-left: auto; 
-`;
