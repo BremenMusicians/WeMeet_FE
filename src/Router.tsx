929c35921310dcdb7ee4f39e90f84b18e7937887
ojinikim <ojinikim@gmail.com>
2025-03-20 23:49:21 +0900

    add :: 채팅 페이지 퍼블리싱

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Header } from "./components/Header";
-import { MyPage } from "./pages/mypage/Mypage";
-import { EditMyPage } from "./pages/mypage/Edit";
-import { Friend } from "./pages/Friend";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Header } from './components/Header'
+import { MyPage } from './pages/mypage/Mypage'
+import { EditMyPage } from './pages/mypage/Edit'
+import { Friend } from './pages/Friend'
+import Chat from './pages/Chat'
 
 export const Router = () => {
-    return (
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/main" element={<MyPage />} />
-                <Route path="/main/edit" element={<EditMyPage />} />
-                <Route path="/friend" element={<Friend />} />
-            </Routes>
-        </BrowserRouter>
-    );
-};
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/main" element={<MyPage />} />
+        <Route path="/main/edit" element={<EditMyPage />} />
+        <Route path="/friend" element={<Friend />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
