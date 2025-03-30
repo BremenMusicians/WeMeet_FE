70e22463fe2197d1e7acb96bf29dffe9b76400c8
HYUNA <phyunaa0525@gmail.com>
2025-03-30 23:25:21 +0900

    feat :: api instance 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,26 @@ import { ThemeProvider } from 'styled-components'
 import { theme } from './styles/Theme'
 import GlobalStyle from './styles/GlobalStyle'
 import { Router } from './Router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 5000,
+      retry: 1,
+    },
+  },
+});
+
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <Router />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
