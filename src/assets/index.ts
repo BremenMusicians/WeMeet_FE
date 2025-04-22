70433304512071713394596fa792b3133d745208
ojinikim <ojinikim@gmail.com>
2025-04-22 11:19:07 +0900

    chore :: 베이스 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -51,6 +51,7 @@ export { default as Snow } from './sticker/snow.svg'
 export { default as Star } from './sticker/star.svg'
 export { default as Double_Down_Arrow } from './Double_Down_Arrow.svg'
 export { default as BaseHeader } from './BaseHeader.webp'
+export * from './Bass'
 
 export interface IconProps {
   Fill?: string
