f0ffbc58e3fd58f16f67d1ecaac73bf514c7d50d
ojinikim <ojinikim@gmail.com>
2025-04-23 15:00:17 +0900

    chore :: svg 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -50,6 +50,7 @@ export { default as Smile } from './sticker/smile.svg'
 export { default as Snow } from './sticker/snow.svg'
 export { default as Star } from './sticker/star.svg'
 export { default as Double_Down_Arrow } from './Double_Down_Arrow.svg'
+export { default as Emoji } from './Emoji.svg'
 export { default as BassHeader } from './BassHeader.webp'
 export * from './Bass'
 
