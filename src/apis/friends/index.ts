b4f2531bbc90b6ee31756a2eab104de2cf2a78cc
HYUNA <phyunaa0525@gmail.com>
2025-04-25 12:07:56 +0900

    feat :: 추천친구 api

--- src/apis/friends/index.ts
+++ src/apis/friends/index.ts
@@ -23,3 +23,24 @@ export const useFriendRequest = () => {
     })
   }
   
+  export const useGetRecommendFriendList = (name: string) => {
+    return useInfiniteQuery({
+        queryKey: ['recommend-friend-list', name],
+        queryFn: async ({ pageParam = 0 }) => {
+          const { data } = await instance.get(`${router}?page=${pageParam}&name=${encodeURIComponent(name)}`)
+          return { ...data, page: pageParam }
+        },
+        getNextPageParam: (lastPage, allPages) => {
+            const totalFetched = allPages.reduce((acc, page) => acc + page.users.length, 0)
+            const totalAvailable = 20
+
+            if (totalFetched < totalAvailable) {
+              return lastPage.page + 1
+            }
+            return undefined
+          },
+        staleTime: 1000 * 60,
+        initialPageParam: 0
+      })
+      
+  }
