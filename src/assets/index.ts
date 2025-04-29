ce4e85fb8b65283e4dccdadf97a8b2d3029306f5
HYUNA <phyunaa0525@gmail.com>
2025-04-29 23:37:46 +0900

    chore :: img url 수정

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -50,7 +50,7 @@ export { default as Smile } from './sticker/smile.svg'
 export { default as Snow } from './sticker/snow.svg'
 export { default as Star } from './sticker/star.svg'
 export { default as Double_Down_Arrow } from './Double_Down_Arrow.svg'
-export { default as BaseHeader } from './BaseHeader.webp'
+export { default as BassHeader } from './BassHeader.webp'
 export * from './Bass'
 
 export interface IconProps {
