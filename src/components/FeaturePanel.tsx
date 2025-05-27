9fcddeb1147eb7bfb9f9504c4eee4ad0dd6387d2
HYUNA <phyunaa0525@gmail.com>
2025-05-27 09:16:10 +0900

    chore :: 악기 선택

--- src/components/FeaturePanel.tsx
+++ src/components/FeaturePanel.tsx
@@ -1,28 +1,32 @@
 import styled from 'styled-components'
 import { Instrument, Volume } from '../assets'
 import { FeatureButton } from './InstrumentButton'
-import { InstrumentList } from './Instrument'
 import { RangeInput } from './RangeCustom'
+import { InstrumentType } from '../utils/type'
+import { InstrumentList } from './Instrument'
 
 interface Props {
   activeFeature: 'instrument' | 'volume' | null
   handleToggleFeature: (type: 'instrument' | 'volume') => void
+  activeInstrument?: InstrumentType
+  handleToggleInstrument?: (instrument: InstrumentType) => void
+  handleChangeVolume?: (volume: number) => void
 }
 
-export const FeaturePanel = ({ activeFeature, handleToggleFeature }: Props) => {
+export const FeaturePanel = ({ activeFeature, handleToggleFeature, activeInstrument, handleChangeVolume, handleToggleInstrument }: Props) => {
   return (
     <>
       <Position>
         <FeatureButton onClick={() => handleToggleFeature('instrument')} isActive={activeFeature === 'instrument'}>
           <Instrument Fill={activeFeature === 'instrument' ? '#F75C3C' : '#3F3F46'} />
         </FeatureButton>
-        {activeFeature === 'instrument' && <InstrumentList activeInstrument="피아노" />}
+        {activeFeature === 'instrument' && <InstrumentList onSelectInstrument={handleToggleInstrument} activeInstrument={activeInstrument} />}
       </Position>
       <Position>
         <FeatureButton onClick={() => handleToggleFeature('volume')} isActive={activeFeature === 'volume'}>
           <Volume Fill={activeFeature === 'volume' ? '#F75C3C' : '#3F3F46'} />
         </FeatureButton>
-        {activeFeature === 'volume' && <RangeInput />}
+        {activeFeature === 'volume' && <RangeInput onChange={handleChangeVolume} />}
       </Position>
     </>
   )
